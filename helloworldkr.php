bb5881ffe565293f352e118dabf290dacf4b9d7b
Tufy4 <tfzinf@gmail.com>
2023-07-28 21:46:07 -0300

    aula-1-PHP

---
+++ helloworldkr.php
@@ -0,0 +1,49 @@
+<?php
+    echo "Hello World <br>";
+// comentario
+# comentario
+/*
+comentario
+comentario
+*/
+
+var_dump(11);
+echo "<br>";
+var_dump(4.5);
+echo "<br>";
+var_dump("rod");
+echo "<br>";
+var_dump(true);
+echo "<br>";
+echo PHP_INT_MAX;
+echo "<br>";
+echo PHP_INT_MIN;
+echo "<br>";
+echo PHP_FLOAT_MAX;
+echo "<br>";
+echo PHP_FLOAT_MIN;
+echo "<br>";
+echo 1+1,"<br>";
+echo 1+2.1,"<br>";
+echo 10 - 2 , "<br>";
+echo 7 / 4, "<br>";
+echo intdiv(7,4), "<br>"; // pega apenas o numero inteiro
+echo round(7/4), "<br>"; // arredonda
+echo 7%4, "<br>";
+// Não pode dividir por zero.
+echo "Kevenn"," Henrique";
+echo "<br>";
+echo "Rodrigo","Henrique";
+echo "<br>";
+echo strtoupper("Rodrigo");
+echo "<br>";
+echo strtolower("KEVENN");
+echo "<br>";
+echo ucfirst("toy <br>");
+echo ucwords("todas as letras <br>");
+echo strlen("Oi").'<br>';
+echo str_replace("isso","aquilo","torcar isso isso");
+
+
+
+?>
